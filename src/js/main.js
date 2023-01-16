f0dd3de70b476eb79a6baaaeee67f11908972ace
Yibo Wang <yw222cb@student.lnu.se>
2023-01-16 18:17:14 +0100

    focus

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,6 @@
+import { makeDraggable } from './app.js'
+import { updateTime } from './time.js'
 setInterval(updateTime, 1000)
-
-/**
- *
- */
-function updateTime () {
-  const date = new Date()
-  const hours = date.getHours().toString().padStart(2, '0')
-  const minutes = date.getMinutes().toString().padStart(2, '0')
-  const seconds = date.getSeconds().toString().padStart(2, '0')
-  document.getElementById('hours').innerText = hours
-  document.getElementById('minutes').innerText = minutes
-  document.getElementById('seconds').innerText = seconds
-}
 
 // Add a click event listener to each app icon
 document.querySelectorAll('.app').forEach((app) => {
@@ -23,7 +12,12 @@ document.querySelectorAll('.app').forEach((app) => {
     windowElement.classList.add('window')
     windowElement.setAttribute('id', appId)
     windowElement.setAttribute('draggable', 'true')
-
+    windowElement.addEventListener('click', () => {
+      document.querySelectorAll('.window.selected').forEach((selectedWindow) => {
+        selectedWindow.classList.remove('selected')
+      })
+      windowElement.classList.add('selected')
+    })
     // Add a title bar to the window element
     const titleBar = document.createElement('div')
     titleBar.classList.add('title-bar')
@@ -45,39 +39,12 @@ document.querySelectorAll('.app').forEach((app) => {
     // Add the title bar to the window element
     windowElement.appendChild(titleBar)
     // Append the window element to the app container
+    // Append the window element to the app container
     document.getElementById('app-container').appendChild(windowElement)
-    windowElement.addEventListener('dragstart', (event) => {
-      console.log('DRAG START', event)
 
-      // Get original position
-      const style = window.getComputedStyle(event.target, null)
-      const startX = parseInt(style.getPropertyValue('left'), 10) - event.clientX
-      const startY = parseInt(style.getPropertyValue('top'), 10) - event.clientY
-      const start = {
-        posX: startX,
-        posY: startY
-      }
-
-      // Save the position in the dataTransfer
-      event.dataTransfer.setData('application/json', JSON.stringify(start))
-      console.log('Start position', start)
-    })
-
-    window.addEventListener('dragover', (event) => {
-      event.preventDefault()
-    })
-
-    window.addEventListener('drop', (event) => {
-      // Get the position of the dragged
-
-      // Get the position of the dragged element and where the drop was
-      const start = JSON.parse(event.dataTransfer.getData('application/json'))
-      const dropX = event.clientX
-      const dropY = event.clientY
-
-      // Move element position from start to drop
-      windowElement.style.left = (dropX + start.posX) + 'px'
-      windowElement.style.top = (dropY + start.posY) + 'px'
-    })
+    // Get the dimensions of the window element
+    // eslint-disable-next-line no-unused-vars
+    const windowRect = windowElement.getBoundingClientRect()
+    makeDraggable(windowElement, windowRect.left, windowRect.top)
   })
 })
