136d5060b191fc41f686bdb517ed75d373234852
Yibo Wang <yw222cb@student.lnu.se>
2023-03-18 22:14:40 +0100

    chat

---
+++ src/apps/chat-app/server.js
@@ -0,0 +1,31 @@
+const WebSocket = require('ws')
+
+const server = new WebSocket.Server({ port: 3000 })
+
+server.on('connection', (socket) => {
+  console.log('Client connected')
+
+  // Broadcast received messages to all connected clients
+  socket.on('message', (message) => {
+    const parsedMessage = JSON.parse(message)
+    console.log(`Received message from ${parsedMessage.username}: ${parsedMessage.text}`)
+
+    const broadcastMessage = JSON.stringify({
+      type: 'message',
+      username: parsedMessage.username,
+      text: parsedMessage.text
+    })
+    console.log(parsedMessage.username)
+    server.clients.forEach((client) => {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        client.send(broadcastMessage)
+      }
+    })
+  })
+
+  socket.on('close', () => {
+    console.log('Client disconnected')
+  })
+})
+
+console.log('WebSocket server is running on ws://localhost:3000')
